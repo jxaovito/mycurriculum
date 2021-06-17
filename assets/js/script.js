8b88b1ed702a2c33d4377c801b1923645afd2b3b
João Vitor <joaovitoorvieiraborba@gmail.com>
2021-06-17 16:37:36 -0300

    script e html parcialmente prontos

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var Sobre = function(){
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+ 
+    divTest1.style.display = "block";
+    divTest2.style.display = "none";
+    
+}
+ 
+var Habilidades = function(){
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+ 
+    divTest1.style.display = "none";
+    divTest2.style.display = "block";
+}
+
+var Hobbies = function(){
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+ 
+    divTest1.style.display = "block";
+    divTest2.style.display = "none";
+}
+
+var Books = function(){
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+ 
+    divTest1.style.display = "none";
+    divTest2.style.display = "block";
+}
