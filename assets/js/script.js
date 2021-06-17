d53ffc23ac2ccdf7fb01e202525e26847381bcdb
João Vitor <joaovitoorvieiraborba@gmail.com>
2021-06-17 17:10:15 -0300

    html e script prontos no modo claro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,70 @@
 var Sobre = function(){
-    divTest1 = document.querySelector('#teste1');
-    divTest2 = document.querySelector('#teste2');
+    div1 = document.querySelector('#teste1');
+    div2 = document.querySelector('#teste2');
+    div3 = document.querySelector('#teste3');
+    div4 = document.querySelector('#teste4');
+    div5 = document.querySelector('#teste5');
  
-    divTest1.style.display = "block";
-    divTest2.style.display = "none";
-    
+    div1.style.display = "block";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
 }
- 
+
 var Habilidades = function(){
-    divTest1 = document.querySelector('#teste1');
-    divTest2 = document.querySelector('#teste2');
+    div1 = document.querySelector('#teste1');
+    div2 = document.querySelector('#teste2');
+    div3 = document.querySelector('#teste3');
+    div4 = document.querySelector('#teste4');
+    div5 = document.querySelector('#teste5');
  
-    divTest1.style.display = "none";
-    divTest2.style.display = "block";
+    div1.style.display = "none";
+    div2.style.display = "block";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+}
+
+var Formacao = function(){
+    div1 = document.querySelector('#teste1');
+    div2 = document.querySelector('#teste2');
+    div3 = document.querySelector('#teste3');
+    div4 = document.querySelector('#teste4');
+    div5 = document.querySelector('#teste5');
+ 
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "none";
+    div5.style.display = "none";
 }
 
 var Hobbies = function(){
-    divTest1 = document.querySelector('#teste1');
-    divTest2 = document.querySelector('#teste2');
+    div1 = document.querySelector('#teste1');
+    div2 = document.querySelector('#teste2');
+    div3 = document.querySelector('#teste3');
+    div4 = document.querySelector('#teste4');
+    div5 = document.querySelector('#teste5');
  
-    divTest1.style.display = "block";
-    divTest2.style.display = "none";
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "block";
+    div5.style.display = "none";
 }
 
 var Books = function(){
-    divTest1 = document.querySelector('#teste1');
-    divTest2 = document.querySelector('#teste2');
+    div1 = document.querySelector('#teste1');
+    div2 = document.querySelector('#teste2');
+    div3 = document.querySelector('#teste3');
+    div4 = document.querySelector('#teste4');
+    div5 = document.querySelector('#teste5');
  
-    divTest1.style.display = "none";
-    divTest2.style.display = "block";
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "block";
 }
+
